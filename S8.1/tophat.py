eaca68dd188734793f44b03359f8478d95a1388f
Ravi Alla <ravi.alla@berkeley.edu>
2014-07-23 11:03:22 -0700

    Wednesday Session 8.1

--- S8.1/tophat.py
+++ S8.1/tophat.py
@@ -1,3 +1,20 @@
 __author__ = 'ravi'
 import glob
+import subprocess as sp
+
+fastq_file_list = glob.glob('*.fastq')
+
+print fastq_file_list
+
+
+program = 'tophat'
+input_files = ','.join(fastq_file_list)
+index = 'E_coli'
+gtf = 'E_coli_mg1655.gtf'
+output_dir = 'TophatOut'
+num_procs = '6'
+
+tophat_out = open('tophat_out','w')
+
+proc = sp.Popen([program,'-G',gtf,'--no-novel-juncs','-o',output_dir,'-p',num_procs,index,input_files],stdout=tophat_out)
 
